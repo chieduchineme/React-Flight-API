9f457f78b909383ae5d6df9aeb3209d5fe30d0f7
ONYEDIKACHUKWU CHIEDU <77061016+chieduchineme@users.noreply.github.com>
2023-05-08 12:02:21 +0100

    Update index.js

--- src_code/src/index.js
+++ src_code/src/index.js
@@ -1,8 +1,18 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-createRoot(document.getElementById("root")).render(<App />);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
